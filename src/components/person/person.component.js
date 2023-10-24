7185420646ac86791177900729e40680d179a76a
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-10-24 17:59:18 +0200

    Add LinkedIn url to PersonComponent

--- src/components/person/person.component.js
+++ src/components/person/person.component.js
@@ -9,9 +9,13 @@ const PersonComponent = (props) => {
         lastname: 'Aubert'
     }
 
+    const linkedIn = <a href="https://www.linkedin.com/in/ideafactory31/" target="_new" title={"Profil LinkedIn de " + singlePerson.firstname + " " +  singlePerson.lastname }>LinkedIn</a>
     const formatName = (person) => <h2>{person.firstname} {person.lastname}</h2>
 
-    return formatName(singlePerson)
+    return <>
+        { formatName(singlePerson) }
+        { linkedIn }
+    </>
 }
 
 export default PersonComponent
