1d8ad0f7c20504bead70eed4840ada32dd05cdff
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-10-24 13:08:40 +0200

    Add HelloComponent to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 
+// Import our custom Component
+import HelloComponent from './components/hello/hello.component'
+
 const App = () => {
   // Sets a constant
   const greeting = 'Hello React'
@@ -9,6 +12,7 @@ const App = () => {
       <header className="App-header">
         <h1>{ greeting }</h1>
       </header>
+      <HelloComponent />
     </div>
   );
 }
---
+++ src/components/hello/hello.component.js
@@ -0,0 +1,10 @@
+/**
+ * HelloComponent
+ *  Simple ReactJS Component that's display a simple text
+ * @author Aélion <jean-luc.aubert@aelion.fr>
+ */
+const HelloComponent = (props) => {
+    return <div>Learn React</div>
+}
+
+export default HelloComponent
