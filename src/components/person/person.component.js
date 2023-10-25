1f1c09ceb2beac55475ea688c78b6027e82e5b46
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-10-25 08:23:08 +0200

    Create a simple HTML list

--- src/components/person/person.component.js
+++ src/components/person/person.component.js
@@ -6,15 +6,28 @@
 const PersonComponent = (props) => {
     const singlePerson = {
         firstname: 'Jean-Luc',
-        lastname: 'Aubert'
+        lastname: 'Aubert',
+        linkedIn: 'https://www.linkedin.com/in/ideafactory31/',
+        facebook: 'https://www.facebook.com/profile.php?id=100070373165180'
     }
 
     const linkedIn = <a href="https://www.linkedin.com/in/ideafactory31/" target="_new" title={"Profil LinkedIn de " + singlePerson.firstname + " " +  singlePerson.lastname }>LinkedIn</a>
     const formatName = (person) => <h2>{person.firstname} {person.lastname}</h2>
-
+    const socialList = <ul>
+        <li>
+            <a href={singlePerson.linkedIn} target="_new" title={`Profil de ${singlePerson.firstname} ${singlePerson.lastname}`}>
+                    LinkedIn
+            </a>
+        </li>
+        <li>
+            <a href={singlePerson.facebook} target="_new" title={`Facebook de ${singlePerson.firstname} ${singlePerson.lastname}`}>
+                    Facebook
+            </a>
+        </li>
+    </ul>
     return <>
         { formatName(singlePerson) }
-        { linkedIn }
+        { socialList }
     </>
 }
 
