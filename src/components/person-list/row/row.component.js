54422e493725f5970aa0ce20980e199f8006df09
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-10-27 15:54:38 +0200

    Add simple onClick handler

--- src/components/person-list/row/row.component.js
+++ src/components/person-list/row/row.component.js
@@ -1,6 +1,11 @@
 const RowComponent = (props) => {
     const person = props.person
 
+    // Set the function that handle onClick event
+    const onClick = () => {
+        console.log(`A button was clicked`)
+    }
+
     return <tr>
         <td>&nbsp;</td>
         <td>{ person.id }</td>
@@ -8,7 +13,9 @@ const RowComponent = (props) => {
         <td>{ person.lastname }</td>
         <td>{ person.email }</td>
         <td>
-            <button type="button">x</button>
+            <button type="button"
+                onClick={onClick}
+            >x</button>
         </td>
     </tr>
 }
