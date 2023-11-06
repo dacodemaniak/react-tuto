8a4998c22daa2673b852ea0e5ff8900da1024394
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-11-06 11:52:33 +0100

    Change toolbar implementation for Redux

--- src/components/person-list/toolbar/toolbar-component.js
+++ src/components/person-list/toolbar/toolbar-component.js
@@ -1,10 +1,18 @@
-const ToolbarComponent = (props) => {
-    return <div className="toolbar">
-        <div className="total-person">
-            <span className="label">Total persons : </span>
-            <span className="total">{ props.total }</span>
+import { useSelector } from 'react-redux'
+
+const ToolbarComponent = () => {
+    const total = useSelector((state) => {
+        return state.personList.personList.length
+    })
+    return (
+        
+            <div className="toolbar">
+                <div className="total-person">
+                    <span className="label">Total persons : </span>
+                    <span className="total">{ total }</span>
+                </div>
         </div>
-    </div>
+    )
 }
 
 export default ToolbarComponent
